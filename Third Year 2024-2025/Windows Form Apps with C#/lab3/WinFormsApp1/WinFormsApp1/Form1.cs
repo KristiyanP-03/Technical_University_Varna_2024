8912769c92b1289f0ac336950c15e80d7f9161c5
Kristiyan Pisev <114045349+KristiyanP-03@users.noreply.github.com>
2024-10-27 08:27:42 +0200

    I forgot to commit my work in university

---
+++ Third Year 2024-2025/Windows Form Apps with C#/lab3/WinFormsApp1/WinFormsApp1/Form1.cs
@@ -0,0 +1,38 @@
+﻿namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void степенуванеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.Show();
+        }
+
+        private void съкратеноУмножениеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form3 form3 = new Form3();
+            form3.Show();
+        }
+
+        private void квадратноУравнениеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form4 form4 = new Form4();
+            form4.Show();
+        }
+
+        private void класToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void математическиФормулиToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Third Year 2024-2025/Windows Form Apps with C#/lab3/WinFormsApp1/WinFormsApp1/Form2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+            form1.Show();
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                double a = double.Parse(textBox1.Text);
+                double b = double.Parse(textBox2.Text);
+
+                double result = Math.Pow((a + b), 2);
+
+                textBox3.Text = result.ToString();
+            }
+            else if (radioButton2.Checked == true)
+            {
+                double a = double.Parse(textBox1.Text);
+                double b = double.Parse(textBox2.Text);
+
+                double result = Math.Pow((a - b), 2);
+
+                textBox3.Text = result.ToString();
+            }
+        }
+    }
+}
